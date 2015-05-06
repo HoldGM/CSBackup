1dd12a5020856f977a1d28faa9619d9a5a812909
Otis <otisbrower@yahoo.com>
2015-05-06 08:07:05 -0500

    project update

--- SDS322/project/C/func.c
+++ SDS322/project/C/func.c
@@ -115,32 +115,32 @@ void heatEq(heat **tempMap, double alpha, int xDim, int yDim){
 
 	//Top left corner
 	if(tempMap[0][0].b == 0){
-		tempMap[0][0].t = newMap[0][0].t + alpha * (newMap[1][0].t + newMap[1][1].t + newMap[0][1].t - 4 * newMap[0][0].t);
+		tempMap[0][0].t = newMap[0][0].t + alpha * (newMap[1][0].t + newMap[1][1].t + newMap[0][1].t - 3 * newMap[0][0].t);
 	}
 	//Lower right corner
 	if(tempMap[xDim-1][yDim-1].b == 0){
 		tempMap[xDim-1][yDim -1].t  = newMap[xDim-1][yDim-1].t + alpha * (newMap[xDim-2][yDim-1].t + newMap[xDim-2][yDim-2].t + 
-			newMap[xDim-1][yDim-2].t - 4 * newMap[xDim-1][yDim-1].t);
+			newMap[xDim-1][yDim-2].t - 3 * newMap[xDim-1][yDim-1].t);
 	}
 	//Lower left Corner
 	if(tempMap[0][yDim-1].b == 0){
 		tempMap[0][yDim-1].t = newMap[0][yDim-1].t + alpha * (newMap[0][yDim-2].t + newMap[1][yDim-2].t + newMap[1][yDim-1].t - 
-			4 * newMap[0][yDim-1].t);
+			3 * newMap[0][yDim-1].t);
 	}
 	//Upper right corner
 	if(tempMap[xDim-1][0].b == 0){
-		tempMap[xDim-1][0].t = newMap[xDim-1][0].t + alpha * (newMap[xDim-2][0].t + newMap[xDim-2][1].t + newMap[xDim-1][1].t - 4 * 
+		tempMap[xDim-1][0].t = newMap[xDim-1][0].t + alpha * (newMap[xDim-2][0].t + newMap[xDim-2][1].t + newMap[xDim-1][1].t - 3 * 
 			newMap[xDim-1][0].t);
 	}
 	//Top edge & Bottom edge
 	for(i = 1; i < xDim - 1; i++){
 		if(tempMap[i][0].b == 0){
 			tempMap[i][0].t = newMap[i][0].t + alpha * (newMap[i-1][0].t + newMap[i-1][1].t + newMap[i][1].t + newMap[i+1][1].t + 
-				newMap[i+1][0].t - 6 * newMap[i][0].t);
+				newMap[i+1][0].t - 5 * newMap[i][0].t);
 		}
 		if(tempMap[i][yDim-1].b == 0){
 			tempMap[i][yDim-1].t = newMap[i][yDim-1].t + alpha * (newMap[i-1][yDim-1].t + newMap[i-1][yDim-2].t + newMap[i][yDim-2].t + 
-				newMap[i +1][yDim-2].t + newMap[i+1][yDim-1].t - 6 * newMap[i][yDim-1].t);
+				newMap[i +1][yDim-2].t + newMap[i+1][yDim-1].t - 5 * newMap[i][yDim-1].t);
 		}
 	}
 
@@ -148,11 +148,11 @@ void heatEq(heat **tempMap, double alpha, int xDim, int yDim){
 	for(j = 1; j < yDim-1; j++){
 		if(tempMap[0][j].b == 0){
 			tempMap[0][j].t = newMap[0][j].t + alpha * (newMap[0][j-1].t + newMap[1][j-1].t + newMap[1][j].t + newMap[1][j+1].t + 
-				newMap[0][j+1].t - 6 * newMap[0][j].t);
+				newMap[0][j+1].t - 5 * newMap[0][j].t);
 		}
 		if(tempMap[xDim-1][j].b == 0){
 			tempMap[xDim-1][j].t = newMap[xDim-1][j].t + alpha * (newMap[xDim-1][j-1].t + newMap[xDim-2][j-1].t + newMap[xDim-2][j].t + 
-				newMap[xDim-2][j+1].t + newMap[xDim-1][j+1].t - 6 * newMap[xDim-1][j].t);
+				newMap[xDim-2][j+1].t + newMap[xDim-1][j+1].t - 5 * newMap[xDim-1][j].t);
 		}
 	}
 
@@ -185,11 +185,10 @@ void initPrintMap(heat **tempMap, int xDim, int yDim, char *out){
 	for( j = 0; j < yDim; j++){
 		for( i = 0; i < xDim; i++){
 			// printf("(%-3d, %-3d) : %-8.4lf  -  %d  ", i, j, tempMap[i][j].t, tempMap[i][j].b);
-			fprintf(output, "(%-3d, %-3d) : %-8.4lf  -  %d  ", i, j, tempMap[i][j].t, tempMap[i][j].b);
+			fprintf(output, "%-3d %-3d : %-8.4lf  -  %d\n", i, j, tempMap[i][j].t, tempMap[i][j].b);
 
 		}
 		// printf("\n");
-		fprintf(output, "\n");
 	}
 	fclose(output);
 }
@@ -212,10 +211,10 @@ void printMap(heat **tempMap, int xDim, int yDim, int numTS, int freq, char *out
 			for( j = 0; j < yDim; j++){
 				for( i = 0; i < xDim; i++){
 					// printf("(%-3d, %-3d) : %-8.4lf  -  %d  ", i, j, tempMap[i][j].t, tempMap[i][j].b);
-					fprintf(output, "(%-3d, %-3d) : %-8.4lf  -  %d  ", i, j, tempMap[i][j].t, tempMap[i][j].b);
+					fprintf(output, "%-3d %-3d : %-8.4lf    %d \n", i, j, tempMap[i][j].t, tempMap[i][j].b);
+
 				}
 				// printf("\n");
-				// fprintf(output, "\n");
 			}
 			fclose(output);
 		}
---
+++ SDS322/project/F/main.f90
@@ -0,0 +1,37 @@
+PROGRAM main
+	implicit none
+	
+	type::heat
+		real*8::t
+		integer::b
+	end type
+	
+	integer :: i, j, k
+	integer :: freq
+	integer :: args
+	character(20) :: out
+	character(20) :: in
+	character(100)::buf
+	
+	character(80) :: filename
+	integer::xDim, yDim, numTS
+	real*8 :: alpha, temp
+	integer :: posX, posY, hold
+	
+	type(heat), allocatable :: tempMap(:,:)
+	
+	args = IARGC()
+	
+	if(args > 0)
+		call getarg(1, in)
+	end if
+	
+	if(args > 1)
+		call getarg(2, numTS)
+	endif
+	
+	if(args > 2)
+		call getarg(3, out)
+	end if
+	
+END PROGRAM main
