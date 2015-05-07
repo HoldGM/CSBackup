b94bb0c4f7926bfb3d25bee5f79a57cd9ec5c413
Otis <otisbrower@yahoo.com>
2015-05-06 21:36:37 -0500

    update fortran project

--- SDS322/project/F/main.f90
+++ SDS322/project/F/main.f90
@@ -1,3 +1,50 @@
+SUBROUTINE wildX(tempMap, posY, xDim, yDim, temp, hold)
+	implicit none
+	
+	type::heat
+		real*8::t
+		integer::b
+	end type
+	
+	integer :: xDim, yDim, posY, hold
+	real*8 :: temp
+	type(heat), dimension(0:xDim, 0:yDim) :: tempMap
+	
+	
+	
+	return
+END SUBROUTINE
+
+SUBROUTINE wildY(tempMap, posX, xDim, yDim, temp, hold)
+	implicit none
+	
+	type::heat
+		real*8::t
+		integer::b
+	end type
+	
+	integer :: xDim, yDim, posX, hold
+	real*8 :: temp
+	type(heat), dimension(0:xDim, 0:yDim) :: tempMap
+	
+	return
+END SUBROUTINE
+
+SUBROUTINE wildXY(tempMap, xDim, yDim, temp, hold)
+	implicit none
+	
+	type::heat
+		real*8::t
+		integer::b
+	end type
+	
+	integer :: xDim, yDim, hold
+	real*8 :: temp
+	type(heat), dimension(0:xDim, 0:yDim) :: tempMap
+	
+	return
+END SUBROUTINE
+
 PROGRAM main
 	implicit none
 	
@@ -6,7 +53,7 @@ PROGRAM main
 		integer::b
 	end type
 	
-	integer :: i, j, k
+	integer :: i, j, k, eof
 	integer :: freq
 	integer :: args
 	character(20) :: out
@@ -39,34 +86,34 @@ PROGRAM main
 	
 	open(10, file = filename)
 	
-	read(10, *) xDim, yDim, alpha, numTS
+	read(10, *, IOSTAT = eof) xDim, yDim, alpha, numTS
 	
 	allocate(tempMap(0:(xDim-1), 0:(yDim-1)))
 	xDim = xDim - 1
 	yDim = yDim - 1
 	
-	!do while (!EOF(10))
-		read(10, *) posX, posY, temp, hold
-		
-		call wildX(tempMap, posY, xDim, yDim, temp, hold)
 	
+	do while (eof > 0)
+		read(10, *, IOSTAT = eof) posX, posY, temp, hold
+		if(posX == ichar('*') .AND. posY /= ichar('*')) then
+			call wildX(tempMap, posY, xDim, yDim, temp, hold)
+		else if ( posY == ichar('*') .AND. posX /= ichar('*'))then
+			call wildY(tempMap, posX, xDim, yDim, temp, hold)
+		else if (posX == ichar('*') .AND. posY == ichar('*')) then
+			call wildXY(tempMap, xDim, yDim, temp, hold)
+		else
+			tempMap(posX,posY)%t = temp
+			tempMap(posX,posY)%b = hold
+		end if
+	end do
 	
+	close(10)
 	
+	do j = 0, yDim
+		do i = 0, xDim
+			print *, i, j, tempMap(i, j)%t
+		end do
+	end do
+	
+	deallocate(tempMap)
 END PROGRAM main
-
-SUBROUTINE wildX(tempMap, posY, xDim, yDim, temp, hold)
-	implicit none
-	
-	type::heat
-		real*8::t
-		integer::b
-	end type
-	
-	integer :: xDim, yDim, posY, hold
-	real*8 :: temp
-	type(heat) :: tempMap(0:xDim, 0:yDim)
-	
-	
-	
-	return
-END SUBROUTINE
