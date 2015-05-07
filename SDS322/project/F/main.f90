1ae44470d97d4ed841f4b2d59aa21cc395c3b953
Otis <otisbrower@yahoo.com>
2015-05-07 00:47:11 -0500

    fortran project update

--- SDS322/project/F/main.f90
+++ SDS322/project/F/main.f90
@@ -1,50 +1,3 @@
-SUBROUTINE wildX(tempMap, posY, xDim, yDim, temp, hold)
-	implicit none
-	
-	type::heat
-		real*8::t
-		integer::b
-	end type
-	
-	integer :: xDim, yDim, posY, hold
-	real*8 :: temp
-	type(heat), dimension(0:xDim, 0:yDim) :: tempMap
-	
-	
-	
-	return
-END SUBROUTINE
-
-SUBROUTINE wildY(tempMap, posX, xDim, yDim, temp, hold)
-	implicit none
-	
-	type::heat
-		real*8::t
-		integer::b
-	end type
-	
-	integer :: xDim, yDim, posX, hold
-	real*8 :: temp
-	type(heat), dimension(0:xDim, 0:yDim) :: tempMap
-	
-	return
-END SUBROUTINE
-
-SUBROUTINE wildXY(tempMap, xDim, yDim, temp, hold)
-	implicit none
-	
-	type::heat
-		real*8::t
-		integer::b
-	end type
-	
-	integer :: xDim, yDim, hold
-	real*8 :: temp
-	type(heat), dimension(0:xDim, 0:yDim) :: tempMap
-	
-	return
-END SUBROUTINE
-
 PROGRAM main
 	implicit none
 	
@@ -53,17 +6,19 @@ PROGRAM main
 		integer::b
 	end type
 	
-	integer :: i, j, k, eof
+	integer :: i, j, k
+	integer :: eof = 100
 	integer :: freq
 	integer :: args
 	character(20) :: out
 	character(20) :: in
 	character(100)::buf
-	
+	integer :: posX, posY
+	character :: tX, tY
 	character(80) :: filename
 	integer::xDim, yDim, numTS
 	real*8 :: alpha, temp
-	integer :: posX, posY, hold
+	integer :: hold, posXint, posYint
 	
 	type(heat), allocatable :: tempMap(:,:)
 	
@@ -83,27 +38,59 @@ PROGRAM main
 	end if
 	
 	write(filename, '("../", A)') in
+	print *, filename
+	open(unit = 10, file = filename)
 	
-	open(10, file = filename)
-	
-	read(10, *, IOSTAT = eof) xDim, yDim, alpha, numTS
+	read(10,*, IOSTAT=eof) xDim, yDim, alpha, numTS
+	print *, xDim, yDim, alpha, numTS
 	
 	allocate(tempMap(0:(xDim-1), 0:(yDim-1)))
 	xDim = xDim - 1
 	yDim = yDim - 1
 	
-	
-	do while (eof > 0)
-		read(10, *, IOSTAT = eof) posX, posY, temp, hold
-		if(posX == ichar('*') .AND. posY /= ichar('*')) then
-			call wildX(tempMap, posY, xDim, yDim, temp, hold)
-		else if ( posY == ichar('*') .AND. posX /= ichar('*'))then
-			call wildY(tempMap, posX, xDim, yDim, temp, hold)
-		else if (posX == ichar('*') .AND. posY == ichar('*')) then
-			call wildXY(tempMap, xDim, yDim, temp, hold)
+	read(10, *, IOSTAT = eof) tX, tY, temp, hold
+	do while ( eof >= 0)
+		print *, eof
+		print *, tX, temp, hold
+		
+		if (tX .eq. '*') then
+			posX = -1
 		else
-			tempMap(posX,posY)%t = temp
-			tempMap(posX,posY)%b = hold
+			read(tX, '(i4)') posX
+		end if
+		
+		if (tY .eq. '*') then
+			posY = -1
+		else
+			read(tY, '(i4)') posY
+		end if
+	
+		print *, posX, posY
+			if(posX< 0 .and. posY >= 0) then
+				do i = 0, xDim
+					tempMap(i, posY)%t = temp
+					tempMap(i, posY)%b = hold
+				end do
+			else if ( posY < 0 .and. posX >= 0)then
+				do i = 0, yDim
+					tempMap(posX, i)%t = temp
+					tempMap(posX, i)%b = hold
+				end do
+			else if (posX < 0 .and. posYint < 0) then
+				do j = 0, yDim
+					do i = 0, xDim
+						tempMap(i, j)%t = temp
+						tempMap(i, j)%b = hold
+					end do
+				end do
+			else
+				tempMap(posXint,posYint)%t = temp
+				tempMap(posXint,posYint)%b = hold
+			end if
+			
+		read(10, *, IOSTAT = eof) posX, posY, temp, hold
+		if(eof < 0) then 
+			EXIT
 		end if
 	end do
 	
@@ -111,7 +98,7 @@ PROGRAM main
 	
 	do j = 0, yDim
 		do i = 0, xDim
-			print *, i, j, tempMap(i, j)%t
+			write (*, *) i, j, tempMap(i, j)%t, numTS
 		end do
 	end do
 	
