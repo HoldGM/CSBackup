753b35922db93dc343c9381acced7ae52feca9b7
Otis <otisbrower@yahoo.com>
2015-09-24 08:09:15 -0500

    working on linked list

--- CS105/assignment4/LinkedList.h
+++ CS105/assignment4/LinkedList.h
@@ -9,51 +9,25 @@
 using namespace std;
 /* your class */
 
-struct Node{
-	Node* next;
-	int* value;
-};
-
-class LinkedList{
+template <typename T>
+class List
+{
+	struct Node
+	{
+		T data;
+		Node *next;
+		Node(T d, Node *n = 0):data(d), next(n) {}
+	};
+	Node *head;
 
 public:
-	int listSize;
-	Node* head;
-	Node* tail;
-
-	void initList(struct Node* newNode, int*n){
-		listSize = 1;
-		(*newNode).value = n;
-		(*newNode).next = NULL;
-		tail = newNode;
-		head = newNode;
-	}
-	void push(struct Node* newNode, int* value){
-		newNode->value = value;
-		newNode->next = NULL;
-		tail->next = newNode;
-		listSize += 1;
-	}
-	Node* pop(){
-		head = head->next;
-
-		return this->head;
-	}
-	int* peek(){
-		return head->value;
-	}
-	void print(){
-		Node* tempNode = head;
-
-		while(tempNode){
-			cout << tempNode->value << " ";
-			tempNode = tempNode->next;
-		}
-		cout << endl;
-	}
-	int size(){
-		return this->listSize;
-	}
+	List(Node *h = 0):head(h){}
+	~List();
+	void insert(Node *loc, T d);
+	void push(T d);
+	T pop();
+	void print();
 };
+
 #endif
 
